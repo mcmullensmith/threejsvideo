8f2ab8e07c2e66561581506f4b03b9657f449b77
Matthew Smith <mcmullensmith@gmail.com>
2017-03-24 14:27:59 -0400

    added transition files and detector file

---
+++ js/TransVideo.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Mattsmith on 3/23/17.
+ */
+var TransVideo = function() {
+    var video;
+
+    function init() {
+
+        video = document.getElementById( 'trans-video' );
+
+        video.src ="textures/2xTransition_1.mp4";
+
+        video.setAttribute('webkit-playsinline', 'webkit-playsinline');
+        video.setAttribute('crossorigin', 'anonymous');
+
+        // video.loop = true;
+        video.load(); // must call after setting/changing source
+        // video.play();
+        // video.autoplay = true;
+        video.setAttribute('crossorigin', 'use-credentials');
+
+        texture = new THREE.VideoTexture( video );
+        texture.minFilter = THREE.LinearFilter;
+        texture.magFilter = THREE.LinearFilter;
+        texture.format = THREE.RGBFormat;
+
+        var movieMaterial = new THREE.MeshBasicMaterial( { map: texture, overdraw: true, side:THREE.DoubleSide } );
+        // the geometry on which the movie will be displayed;
+        // 		movie image will be scaled to fit these dimensions.
+        var movieGeometry = new THREE.PlaneGeometry( 1440, 900, 0, 0 );
+        var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
+        movieScreen.position.set(0,0,0);
+
+        Transition.getScene().add(movieScreen);
+        Transition.getCamera().lookAt(movieScreen.position);
+    }
+
+    return {
+        init: init,
+        getVideo: function() {
+            return video;
+        }
+    }
+}();
---
+++ js/Transition.js
@@ -0,0 +1,49 @@
+var Transition = function () {
+
+    var scene, camera, renderer, light;
+
+    function init() {
+        events.on("update", update);
+
+        container = document.getElementById( 'transition-video' );
+        document.body.appendChild( container );
+
+
+        camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+        camera.position.z = 1000;
+
+        scene = new THREE.Scene();
+
+        scene.add(camera);
+
+        light = new THREE.DirectionalLight( 0xffffff );
+        light.position.set( 0.5, 1, 1 ).normalize();
+        scene.add( light );
+
+        var canvas = document.getElementById("transition");
+        renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: false, alpha: true });
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        container.appendChild( renderer.domElement );
+    }
+
+    function update() {
+        renderer.render( scene, camera );
+    }
+
+    return {
+        init: init,
+        getCamera: function () {
+            return camera;
+        },
+        getScene: function () {
+            return scene;
+        },
+        getLight: function () {
+            return light;
+        },
+        getRenderer: function () {
+            return renderer;
+        }
+    }
+}();
